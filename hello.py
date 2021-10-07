4626e1c0cf113d4ba1e08e31742fd67e273df340
jogarao123 <hello@amigoscode.com>
2021-10-07 12:49:03 +0530

    random try

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user_page(name):
+    return render_template('user.html',username=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "not fud"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
